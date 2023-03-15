1c64ee150ad738fad6e4f7ea0df38705e9ff0073
Gary Powell <54749261+garypow@users.noreply.github.com>
2023-03-15 11:59:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function before()
+{
+  document.getElementById('myImage')
+  .src="images/ppl.jpg";
+  document.getElementById('message')
+  .innerHTML="Howzit Exsay!";
+}
+
+function after()
+{
+  document.getElementById('myImage')
+  .src="images/logbook.jpg";
+  document.getElementById('message')
+  .innerHTML="Cherio Exsay!";
+}
